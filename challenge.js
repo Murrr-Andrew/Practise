de89bdb21c8ab7d1359caefd949118f185784be8
Andrew Murrow <murrr.andrew@gmail.com>
2023-12-26 21:35:38 +0100

    practise/challenge-5

--- challenge.js
+++ challenge.js
@@ -2,3 +2,4 @@
 // 2) https://codesandbox.io/p/sandbox/crazy-perlman-rl3t8w?file=%2Fsrc%2FApp.js%3A11%2C21
 // 3) https://codesandbox.io/p/sandbox/charming-river-432h58?file=%2Fsrc%2FApp.js%3A51%2C1
 // 4) https://codesandbox.io/p/sandbox/confident-colden-dnvpsl?file=%2Fsrc%2FApp.js%3A50%2C42
+// 5) https://codesandbox.io/p/sandbox/intelligent-wescoff-zy564s?file=%2Fsrc%2FApp.js%3A30%2C29
