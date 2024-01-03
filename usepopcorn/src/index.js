3cf780ca743149cf4ff31b0ae6f4815dec85d905
Andrew Murrow <murrr.andrew@gmail.com>
2024-01-03 11:05:37 +0100

    practise/project-5

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 
 import StartRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StartRating color="blue" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating maxRating={5} />
+    <StartRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}/>
+    <StartRating size={16} className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
