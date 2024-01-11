be8546c90859b54a9fef7ed95c6f696aa57f7c62
Andrew Murrow <murrr.andrew@gmail.com>
2024-01-11 20:26:53 +0100

    practise: custom hook - useMovies

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import StarRating from "./StarRating";
+import { useMovies } from "./useMovies";
 
 const tempMovieData = [
   {
@@ -403,20 +404,18 @@ function Main({ children }) {
 const KEY = "52bfdea9";
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
 
-  // const [watched, setWatched] = useState([]);
+  const { movies, isLoading, error } = useMovies(query);
+
+  // handleCloseMovie();
+
   const [watched, setWatched] = useState(function() {
     const storedValue = localStorage.getItem("watched");
 
     return JSON.parse(storedValue) || [];
   });
-
-  const tempQuery = "interstellar";
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => id === selectedId ? null : id);
@@ -433,52 +432,6 @@ export default function App() {
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
   }
-
-  useEffect(function() {
-    const controller = new AbortController();
-
-    async function fetchMovies() {
-      try {
-        setIsLoading(true);
-        setError("");
-
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-          { signal: controller.signal }
-        );
-
-        if (!res.ok) throw new Error("Something went wrong with fetching movies");
-
-        const data = await res.json();
-
-        if (data.Response === "False") throw new Error("Movie not found");
-
-        setMovies(data.Search);
-        setIsLoading(false);
-        setError("");
-      } catch (err) {
-        if (err.name !== "AbortError") {
-          setError(err.message);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    if (query.length < 2) {
-      setMovies([]);
-      setError("");
-      return;
-    }
-
-    handleCloseMovie();
-    fetchMovies();
-
-    // cleanup function
-    return function() {
-      controller.abort();
-    }
-  }, [query]);
 
   useEffect(function() {
     localStorage.setItem("watched", JSON.stringify(watched));
---
+++ usepopcorn/src/useMovies.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+const KEY = "52bfdea9";
+
+export function useMovies(query) {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(
+        function () {
+            const controller = new AbortController();
+
+            async function fetchMovies() {
+                try {
+                setIsLoading(true);
+                setError("");
+
+                const res = await fetch(
+                    `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                    { signal: controller.signal }
+                );
+
+                if (!res.ok)
+                    throw new Error("Something went wrong with fetching movies");
+
+                const data = await res.json();
+
+                if (data.Response === "False") throw new Error("Movie not found");
+
+                setMovies(data.Search);
+                setIsLoading(false);
+                setError("");
+                } catch (err) {
+                if (err.name !== "AbortError") {
+                    setError(err.message);
+                }
+                } finally {
+                setIsLoading(false);
+                }
+            };
+
+            if (query.length < 2) {
+                setMovies([]);
+                setError("");
+                return;
+            }
+
+            fetchMovies();
+
+            // cleanup function
+            return function () {
+                controller.abort();
+            };
+        },
+        [query]
+    );
+
+    return {movies, isLoading, error};
+}
