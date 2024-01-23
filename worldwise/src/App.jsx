0cd0ee5e008b93b82f5f172d6f3b73e98d500500
Andrew Murrow <murrr.andrew@gmail.com>
2024-01-23 22:27:13 +0100

    practise: project-9

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- worldwise/src/pages/Homepage.jsx
+++ worldwise/src/pages/Homepage.jsx
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
-function Homepage() {
-    return (
-        <div>
-            <PageNav />
-            <h1>WorldWise</h1>
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <PageNav />
 
-            <Link to="/app">Go to the app</Link>
-        </div>
-    )
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+
+        <Link to="/app" className="cta">
+          Start tracking now
+        </Link>
+      </section>
+    </main>
+  );
 }
-
-export default Homepage;
