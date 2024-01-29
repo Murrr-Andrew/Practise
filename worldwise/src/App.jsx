392a94a1abed0b7af936238993089d0b2d263093
Andrew Murrow <murrr.andrew@gmail.com>
2024-01-29 14:31:21 +0100

    practise: useNavigate hook

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -9,6 +9,7 @@ import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import City from "./components/City";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
 import PageNotFound from "./pages/PageNotFound";
 
 const BASE_URL = "http://localhost:3021";
@@ -59,7 +60,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading}  />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
