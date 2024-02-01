a0483c702211568f06e2c517ab5d71a3592a377d
Andrew Murrow <murrr.andrew@gmail.com>
2024-02-01 16:20:23 +0100

    practise: useNavigate, useCities, async requests from Context

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import { useCities } from "../../contexts/CitiesContext";
+
 import styles from "./CityItem.module.css";
 
 const formatDate = (date) =>
@@ -10,18 +12,22 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-    const { cityName, emoji, date, id, position } = city;
+  const { currentCity } = useCities();
+  const { cityName, emoji, date, id, position } = city;
 
-    return (
-      <li>
-        <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
-          <span className={styles.emoji}>{emoji}</span>
-          <h3 className={styles.name}>{cityName}</h3>
-          <time className={styles.date}>({formatDate(date)})</time>
-          <button className={styles.deleteBtn}>&times;</button>
-        </Link>
-      </li>
-    );
+  return (
+    <li>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={`${styles.cityItem} ${currentCity.id === id ? styles['cityItem--active'] : "" }`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
+    </li>
+  );
 }
 
 export default CityItem;
