bbbedb9eb22f905534d6318d01c93793d05b03b9
Andrew Murrow <murrr.andrew@gmail.com>
2024-02-22 17:19:02 +0100

    practise: worldwise base structure

---
+++ worldwise/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ worldwise/src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>404</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
