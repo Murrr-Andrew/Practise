af85c9994ddc648570e603d1588d06510748bd7e
Andrew Murrow <murrr.andrew@gmail.com>
2024-03-11 20:55:39 +0100

    practise: useNavigation hook for getting page status

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,21 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
+import Loader from "./Loader";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
 
 function AppLayout() {
-    return (
-      <div>
-        <Header />
+  // Hook to know the status of the page (loading / idle)
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
-        <main>
-          <h1>Content</h1>
-          <Outlet />
-        </main>
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
 
-        <CartOverview />
-      </div>
-    );
+      <Header />
+
+      <main>
+        <h1>Content</h1>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
