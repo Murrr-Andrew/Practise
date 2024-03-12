59615262a54536b217ba73d568b9fe056e03d529
Andrew Murrow <murrr.andrew@gmail.com>
2024-03-12 19:37:15 +0100

    practise: tailwind practise

---
+++ fast-react-pizza/prettier.config.cjs
@@ -0,0 +1,5 @@
+/* eslint-disable */
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
+/* eslint-enable */
--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
 
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
-    return (
-        <header>
-            <Link to="/">Fast React Pizza Co.</Link>
+  return (
+    <header className="border-b border-stone-200 bg-yellow-400 px-4 py-3">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
-            <SearchOrder />
-        </header>
-    )
+      <SearchOrder />
+
+      <Username />
+    </header>
+  );
 }
 
 export default Header;
